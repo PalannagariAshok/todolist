e215718c9cc8f377b5a56db0c2425d551c5b6e78
PalannagariAshok <ashok@tridemobility.com>
2023-09-23 23:24:59 +0530

    trains api calls

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import axios from "axios";
 import { Input } from "./components/Common-fields/input";
 function App() {
   const [username, setUsername] = useState(null);
-
+  const [companyname, setCompanyname] = useState(null);
+  const [ownerEmail, setOwnerEmail] = useState(null);
+  const [rollNo, setRollNo] = useState(null);
+  const [accessCode, setAccessCode] = useState(null);
+  const [clientID, setclientID] = useState(null);
+  const [clientSecret, setclientSecret] = useState(null);
   const [dataSet, setDataSet] = useState([]);
 
   const [actionName, setActionName] = useState("Add");
@@ -14,12 +19,8 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://dummyjson.com/products").then((el) => {
-      console.log(el.data.products);
-      setProducts(el.data.products);
-    });
-  }, []);
+  const [trains, setTrains] = useState([]);
+  useEffect(() => {}, []);
 
   useEffect(() => {
     console.log([{ age: 40 }, { age: 20 }].filter((value) => value.age === 40));
@@ -27,66 +28,79 @@ function App() {
 
   return (
     <div className="App">
-      <Input value={username} setValue={setUsername} type="text" />
-
+      <div>
+        <label>Company Name</label>{" "}
+        <Input value={companyname} setValue={setCompanyname} type="text" />
+      </div>
+      <div>
+        <label>Owner Name</label>{" "}
+        <Input value={username} setValue={setUsername} type="text" />
+      </div>
+      <div>
+        <label>Roll No</label>{" "}
+        <Input value={rollNo} setValue={setRollNo} type="text" />
+      </div>
+      <div>
+        <label>Owner Email</label>{" "}
+        <Input value={ownerEmail} setValue={setOwnerEmail} type="text" />
+      </div>
+      <div>
+        <label>Access Code</label>{" "}
+        <Input value={accessCode} setValue={setAccessCode} type="text" />
+      </div>
       <button
         onClick={() => {
           axios
-            .post("https://dummyjson.com/posts/add", {
-              title: username,
-              userId: 5,
+            .post("http://20.244.56.144/train/register", {
+              companyName: companyname,
+              ownerName: username,
+              rollNo: rollNo,
+              ownerEmail: ownerEmail,
+              accessCode: accessCode,
             })
             .then((el) => {
               console.log("post", el.data);
+              setclientID(el.data.clientID);
+              setclientSecret(el.data.clientSecret);
+              axios
+                .post("http://20.244.56.144/train/auth", {
+                  companyName: companyname,
+                  ownerName: username,
+                  rollNo: rollNo,
+                  ownerEmail: ownerEmail,
+                  clientID: el.data.clientID,
+                  clientSecret: el.data.clientSecret,
+                })
+                .then((value) => {
+                  axios.defaults.headers.common[
+                    "Authorization"
+                  ] = `Bearer ${value.data.access_token}`;
+                })
+                .catch((el) => {
+                  alert(el.message);
+                });
+            })
+            .catch((el) => {
+              alert(el.message);
             });
-
-          if (actionName === "Add") {
-            setDataSet([
-              ...dataSet,
-              { name: username, age: 18, branch: "CSE" },
-            ]);
-            setUsername("");
-            setActionName("Add");
-          } else if (actionName === "Edit") {
-            console.log(indexValue);
-            setDataSet((prev) => [
-              ...prev.map((el, i) => {
-                if (i == indexValue) {
-                  return { name: username, age: 18, branch: "CSE" };
-                } else {
-                  return el;
-                }
-              }),
-            ]);
-            setUsername("");
-            setActionName("Add");
-          }
         }}
       >
-        {actionName}
+        register
       </button>
 
-      {dataSet.map((value, index) => (
-        <div key={index}>
-          {value.name},{value.branch},{index}{" "}
-          <button
-            onClick={() => {
-              setDataSet((prev) => prev.filter((element, i) => i !== index));
-            }}
-          >
-            delete
-          </button>
-          <button
-            onClick={() => {
-              setUsername(value.name);
-              setActionName("Edit");
-              setIndexValue(index);
-            }}
-          >
-            edit
-          </button>
-        </div>
-      ))}
+      {clientID !== null && (
+        <button
+          onClick={() => {
+            axios.get("http://20.244.56.144/train/trains").then((el) => {
+              console.log(el);
+              setTrains(el.data);
+            });
+          }}
+        >
+          View Trains
+        </button>
+      )}
+
       <div
         style={{
           display: "flex",
@@ -95,18 +109,25 @@ function App() {
           justifyContent: "space-evenly",
         }}
       >
-        {products
-          .filter((el) => el.price >= 1000)
-          .sort((a, b) => a.title.localeCompare(b.title))
-          .map((value, index) => (
-            <div style={{ border: "1px solid black" }} key={index}>
-              <img
-                src={value.images[0]}
-                style={{ height: "80px", width: "100px" }}
-              />
-              <h5>{value.title}</h5>
-            </div>
-          ))}
+        {trains.map((el) => (
+          <div
+            style={{
+              width: "100px",
+              height: "100px",
+              border: "1px solid black",
+            }}
+            onClick={() => {
+              axios
+                .get("http://20.244.56.144/train/trains/" + el.trainNumber)
+                .then((el) => {
+                  console.log("single train", el);
+                });
+            }}
+          >
+            train name:{el.trainName}
+            <div>train no:{el.trainNumber}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
